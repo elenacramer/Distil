d3d01ad8d5b9cc98e76746997c2e177aa661bccf
elenacramer <elenascramer@gmail.com>
2021-01-12 16:29:52 +0100

    XLM distilliation to LSTM

--- src/distil_xlm.py
+++ src/distil_xlm.py
@@ -13,47 +13,47 @@ from xlm_trainer import batch_to_inputs
 from lstm_trainer import LSTMDistilled
 from utils import get_logger, device, set_seed
 
-if __name__ == '__main__':
-    logger = get_logger()
 
-    set_seed(3)
+logger = get_logger()
 
-    # 1. get data
-    train_df = pd.read_csv(TRAIN_FILE, encoding='utf-8', sep='\t')
+set_seed(3)
 
-    xlm_model = XLMForSequenceClassification.from_pretrained('xlm-mlm-en-2048')
-    tokenizer = XLMTokenizer.from_pretrained('xlm-mlm-en-2048')
+# 1. get data and teacher model
+train_df = pd.read_csv(TRAIN_FILE, encoding='utf-8', sep='\t')
 
-    train_dataset = df_to_dataset(train_df, tokenizer, distillation_settings['max_seq_length'])
-    sampler = SequentialSampler(train_dataset)
-    data = DataLoader(train_dataset, sampler=sampler, batch_size=distillation_settings['train_batch_size'])
+xlm_model = XLMForSequenceClassification.from_pretrained('xlm-mlm-en-2048')
+tokenizer = XLMTokenizer.from_pretrained('xlm-mlm-en-2048')
 
-    xlm_model.to(device())
-    xlm_model.eval()
+train_dataset = df_to_dataset(train_df, tokenizer, distillation_settings['max_seq_length'])
+sampler = SequentialSampler(train_dataset)
+data = DataLoader(train_dataset, sampler=sampler, batch_size=distillation_settings['train_batch_size'])
 
-    xlm_logits = None
+xlm_model.to(device())
+xlm_model.eval()
 
-    for batch in tqdm(data, desc="xlm logits"):
-        batch = tuple(t.to(device()) for t in batch)
-        inputs = batch_to_inputs(batch)
+xlm_logits = None
 
-        with torch.no_grad():
-            outputs = xlm_model(**inputs)
-            _, logits = outputs[:2]
+for batch in tqdm(data, desc="xlm logits"):
+    batch = tuple(t.to(device()) for t in batch)
+    inputs = batch_to_inputs(batch)
 
-            logits = logits.cpu().numpy()
-            if xlm_logits is None:
-                xlm_logits = logits
-            else:
-                xlm_logits = np.vstack((xlm_logits, logits))
+    with torch.no_grad():
+        outputs = xlm_model(**inputs)
+        _, logits = outputs[:2]
 
-    # 2.
-    X_train = train_df['sentence'].values
-    y_train = xlm_logits
-    y_real = train_df['label'].values
+        logits = logits.cpu().numpy()
+        if xlm_logits is None:
+            xlm_logits = logits
+        else:
+            xlm_logits = np.vstack((xlm_logits, logits))
 
-    # 3. trainer
-    distiller = LSTMDistilled(distillation_settings, logger)
+# 2.
+X_train = train_df['sentence'].values
+y_train = xlm_logits
+y_real = train_df['label'].values
 
-    # 4. train
-    model, vocab = distiller.train(X_train, y_train, y_real, ROOT_DATA_PATH)
+# 3. trainer
+distiller = LSTMDistilled(distillation_settings, logger)
+
+# 4. train
+model, vocab = distiller.train(X_train, y_train, y_real, ROOT_DATA_PATH)
